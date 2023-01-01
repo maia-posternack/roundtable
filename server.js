975c4b4e7cb023b12bd22ffe425c14d319113d87
maiaposternack22 <maiaposternack@gmail.com>
2023-01-01 01:34:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
- 
 
+ 
 const ejs = require('ejs');
 const fs = require('fs');
 const methodOverride = require('method-override');
